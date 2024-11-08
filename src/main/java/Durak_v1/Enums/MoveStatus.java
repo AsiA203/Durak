0160b0d39d700681b681228758b9a46ad87d73d6
AsiA203 <aselia.azimkanova@gmail.com>
2024-11-07 20:42:19 -0600

    Created new enum MoveStatus, added couple most needed statuses

---
+++ src/main/java/Durak_v1/Enums/MoveStatus.java
@@ -0,0 +1,18 @@
+package Durak_v1.Enums;
+
+public enum MoveStatus {
+    MOVE_VALID("Move is valid"),
+    MOVE_INVALID("Move is invalid"),
+    INPUT_FORMAT_ERROR("Format of input move is not correct. Please follow this example-> ");
+    //TODO INPUT_FORMAT_ERROR
+
+    private final String message;
+
+    MoveStatus(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
