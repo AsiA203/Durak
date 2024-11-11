af369ca0025fc93527f231679993992b2df5a92b
AsiA203 <aselia.azimkanova@gmail.com>
2024-11-10 21:06:48 -0600

    Added Getters, Setters and Constructor to Card class

--- src/main/java/Durak_v1/Model/Card.java
+++ src/main/java/Durak_v1/Model/Card.java
@@ -7,4 +7,27 @@ public class Card {
     int type;
     String name;
     Color color;
+
+    public Card(int id, int type, String name, Color color) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
